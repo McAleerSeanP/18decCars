70f734aeb3bb58844e9f953486ce142de3faace1
Sean McAleer <L00150213@student.lyit.ie>
2018-12-19 16:52:02 +0000

    Minor edits to buttons at bottom

--- Electric/ElectricUI/MainWindow.xaml.cs
+++ Electric/ElectricUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,43 @@ namespace ElectricUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        electricEntities db = new electricEntities("metadata=res://*/ElectricModel.csdl|res://*/ElectricModel.ssdl|res://*/ElectricModel.msl;provider=System.Data.SqlClient;provider connection string='data source=smcaleer-hp-355;initial catalog=electric;persist security info=True;user id=electric;password=electricPassw0rd!;MultipleActiveResultSets=True;App=EntityFramework'" );
+   
         public MainWindow()
         {
             InitializeComponent();
+        
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string currentUser = tbxLoginUserName.Text;
+            string currentPassword = tbxLoginPassword.Password;
+            foreach (var user in db.Users)
+            {
+                if (user.UserName == currentUser && user.Password == currentPassword)
+                {
+                    MessageBox.Show("User Authenticated");
+                }
+                else
+                {
+                    lblLoginErrorMessage.Content = "Please check your username and password.";
+                }
+                
+            }
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Register RegisterWindow = new Register();
+            RegisterWindow.Show();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            //Shutdown the application
+            Application.Current.Shutdown();
         }
     }
 }
